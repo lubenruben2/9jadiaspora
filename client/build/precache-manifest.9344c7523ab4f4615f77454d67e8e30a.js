383d497fe0ee9643902879ec38ab621dcbddb628
emmanuel <lubenruben2@gmail.com>
2021-02-25 21:52:59 -0800

    update

---
+++ client/build/precache-manifest.9344c7523ab4f4615f77454d67e8e30a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ba78b91ade03e01941ac5e64eed18d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8699a852cf817c0ebad4",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2f3bc74a60321bd5913c",
+    "url": "/static/css/main.ffa2a2bb.chunk.css"
+  },
+  {
+    "revision": "8699a852cf817c0ebad4",
+    "url": "/static/js/2.17c47cd5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.17c47cd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f3bc74a60321bd5913c",
+    "url": "/static/js/main.ee053122.chunk.js"
+  },
+  {
+    "revision": "4fb080de24627b03831e",
+    "url": "/static/js/runtime-main.f16ea130.js"
+  },
+  {
+    "revision": "9ed90dfb3f97b8da6455fa4ee188920f",
+    "url": "/static/media/Banner.9ed90dfb.jpg"
+  },
+  {
+    "revision": "167fe49e203847e6646d3496759436c6",
+    "url": "/static/media/Cars-Background-PNG.167fe49e.png"
+  },
+  {
+    "revision": "ee92a9dc55bf42388838437316bd6bf7",
+    "url": "/static/media/Sponsors.ee92a9dc.jpg"
+  },
+  {
+    "revision": "2122c1efb48b7e8fc813ffa6e5ade058",
+    "url": "/static/media/car1.2122c1ef.jpg"
+  },
+  {
+    "revision": "22f5bb2a6849ae29ff03eecee7627013",
+    "url": "/static/media/car11.22f5bb2a.jpg"
+  },
+  {
+    "revision": "9290da1ae004fc105f93497b0ebfe4ba",
+    "url": "/static/media/car2.9290da1a.jpg"
+  },
+  {
+    "revision": "ef4f0810089130eee9760671316c021e",
+    "url": "/static/media/car22.ef4f0810.jpg"
+  },
+  {
+    "revision": "e6dbda94642ad16c917de3ca217b1e13",
+    "url": "/static/media/car3.e6dbda94.jpg"
+  },
+  {
+    "revision": "3bda2236df69dfc2efee9fdfbe0fe8fc",
+    "url": "/static/media/car333.3bda2236.jpg"
+  },
+  {
+    "revision": "6b60cde874010eb9bf093bca5878812b",
+    "url": "/static/media/contact-us.6b60cde8.png"
+  },
+  {
+    "revision": "b444245575ceda1bed8928c1441436e8",
+    "url": "/static/media/eventimg1.b4442455.jpg"
+  },
+  {
+    "revision": "d6e3d7eb1b8dafe774ff0b13c6b4aa01",
+    "url": "/static/media/image1.d6e3d7eb.jpg"
+  },
+  {
+    "revision": "795f49b9949847c84b8516d285ff06d6",
+    "url": "/static/media/news.795f49b9.jpg"
+  },
+  {
+    "revision": "05213b1d3ad65b1b28fb3c0d14044ce8",
+    "url": "/static/media/world.05213b1d.jpg"
+  }
+]);
